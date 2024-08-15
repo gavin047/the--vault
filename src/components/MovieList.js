bf2902785fd43bc667e0d283be05838422756fb1
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-08-15 13:14:49 +0300

    done

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,23 +1,35 @@
 import React from 'react';
 
-const MovieList = (props) => {
-	const FavouriteComponent = props.favouriteComponent;
-
-	return (
-		<>
-			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
-					<div
-						onClick={() => props.handleFavouritesClick(movie)}
-						className='overlay d-flex align-items-center justify-content-center'
-					>
-						<FavouriteComponent />
-					</div>
-				</div>
-			))}
-		</>
-	);
+const MovieList = ({ movies, handleFavouritesClick, favouriteComponent: FavouriteComponent, handleMovieClick }) => {
+  return (
+    <div className='movie-list'>
+      {movies.map((movie, index) => (
+        <div key={index} className='movie-item'>
+          <img 
+            src={movie.Poster} 
+            alt={movie.Title} 
+            onClick={() => handleMovieClick(movie)}
+          />
+          <div>
+            <h5>{movie.Title}</h5>
+            <p>{movie.Year}</p>
+            <div>
+              <button 
+                onClick={() => handleMovieClick(movie)} 
+                className='btn btn-info'
+              >
+                View Details
+              </button>
+              <FavouriteComponent 
+                movie={movie} 
+                handleFavouritesClick={handleFavouritesClick}
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MovieList;
